b8e0bebbfcc780a79792c0cd1d29814e5eae7542
jabjam <jacobin@berkeley.edu>
2024-12-02 18:19:41 -0800

    Update Anderson_CML_Best.thy

--- Anderson_CML_Best.thy
+++ Anderson_CML_Best.thy
@@ -1,113 +1,137 @@
-theory Anderson_CML
+theory Anderson_CML_Best
 imports Main "../CFML_Lewis_var"
 
 begin
 
 
-section \<open>* Taylor's Ontological Argument -- Counterfactuals *\<close>
+section ‹* Taylor's Ontological Argument -- Counterfactuals *›
 
-consts P :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>"  
+consts P :: "(μ ⇒ σ) ⇒ σ"  
   
-definition G :: "\<mu> \<Rightarrow> \<sigma>" where 
-            "G x \<equiv> \<^bold>\<forall>\<Phi>.( P \<Phi> \<^bold>\<leftrightarrow>  ( (\<^bold>\<box> (\<Phi> x ))) )"  
+definition G :: "μ ⇒ σ" where 
+            "G x ≡ ❙∀Φ.( P Φ ❙↔  ( (❙□ (Φ x ))) )"  
             
-definition NotEq :: "\<mu> \<Rightarrow> \<mu> \<Rightarrow> \<sigma>" where
-  "NotEq x y \<equiv> \<^bold>\<not>(x \<^bold>=\<^sup>L y)"
+definition NotEq :: "μ ⇒ μ ⇒ σ" where
+  "NotEq x y ≡ ❙¬(x ❙=⇧L y)"
   
-consts H :: "\<mu> \<Rightarrow> \<sigma>"
-type_synonym z = " \<mu> \<Rightarrow> \<sigma>"
+consts H :: "μ ⇒ σ"
+type_synonym z = " μ ⇒ σ"
   
 axiomatization where
     CF1: "Preorder" and
-    CF2: "\<forall>w. Total(w)" and
-    CF3: "LewisVC" and
-    CF4: "\<forall>w. {w} = Lew_Minset w (\<^bold>\<not> \<bottom>)"
+    CF2: "∀w. Total(w)" and
+    CF3: "LewisVC"
+
 
 (* axiomatization where
-    A1:  "\<lfloor>\<^bold>\<forall>\<Phi>. (((P \<Phi>) \<^bold>\<rightarrow> \<^bold>\<not> (P (\<^sup>\<not>\<Phi>)) )  )\<rfloor>"  and
-    A2:  "\<lfloor>\<^bold>\<forall>\<Phi>.( \<^bold>\<forall>\<Psi>.( ( (P \<Phi>) \<^bold>\<and> \<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( \<Phi> x \<^bold>\<rightarrow> \<Psi> x))) \<^bold>\<rightarrow> P \<Psi>))\<rfloor>" and
-    A3:  "\<lfloor>P G\<rfloor>" and
-    B3:  "\<lfloor>P H\<rfloor>" *)
+    A1:  "⌊❙∀Φ. (((P Φ) ❙→ ❙¬ (P (⇧¬Φ)) )  )⌋"  and
+    A2:  "⌊❙∀Φ.( ❙∀Ψ.( ( (P Φ) ❙∧ ❙□ (❙∀⇧Ex.( Φ x ❙→ Ψ x))) ❙→ P Ψ))⌋" and
+    A3:  "⌊P G⌋" and
+    B3:  "⌊P H⌋" *)
 
 
-consts D :: "\<mu> \<Rightarrow> \<sigma>" 
+consts D :: "μ ⇒ σ" 
   
-definition DL :: "\<mu> \<Rightarrow> \<sigma>" where
-  " DL (x) \<equiv> \<^bold>\<not>(D(x))"  
+definition DL :: "μ ⇒ σ" where
+  " DL (x) ≡ ❙¬(D(x))"  
     
-definition Perfective1 ::  "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where 
-    "  Perfective1 (\<phi>)  \<equiv> ( (\<^bold>\<forall>\<^sup>Ex.( (\<^bold>\<not> (\<phi>(x))) \<box>\<rightarrow>  D(x) ) ))"  
+definition Perfective1 ::  "(μ ⇒ σ) ⇒ σ" where 
+    "  Perfective1 (φ)  ≡ ( (❙∀⇧Ex.( (❙¬ (φ(x))) □→  D(x) ) ))"  
     
-definition Perfective2 ::  "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where 
-    "  Perfective2 (\<phi>)  \<equiv>  \<^bold>\<not>( \<^bold>\<exists>\<^sup>Ex.( \<phi>(x) \<box>\<rightarrow> D(x) ))"      
+definition Perfective2 ::  "(μ ⇒ σ) ⇒ σ" where 
+    "  Perfective2 (φ)  ≡  ❙¬( ❙∃⇧Ex.( φ(x) □→ D(x) ))"      
   
-definition Perfective ::  "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where 
-    "  Perfective (\<phi>)  \<equiv> ( (\<^bold>\<forall>\<^sup>Ex.( (\<^bold>\<not> (\<phi>(x))) \<box>\<rightarrow>  D(x) ) ))  \<^bold>\<and>    ( \<^bold>\<not>( \<^bold>\<exists>\<^sup>Ex.( \<phi>(x) \<box>\<rightarrow> D(x) )))"
+definition Perfective ::  "(μ ⇒ σ) ⇒ σ" where 
+    "  Perfective (φ)  ≡ ( (❙∀⇧Ex.( (❙¬ (φ(x))) □→  D(x) ) ))  ❙∧    ( ❙¬( ❙∃⇧Ex.( φ(x) □→ D(x) )))"
 
-lemma PerfConj : "\<lfloor>Perfective(\<phi>) \<^bold>\<leftrightarrow>  (Perfective1(\<phi>)  \<^bold>\<and> Perfective2(\<phi>))\<rfloor> "
+lemma PerfConj : "⌊Perfective(φ) ❙↔  (Perfective1(φ)  ❙∧ Perfective2(φ))⌋ "
   apply (metis Perfective_def Perfective1_def Perfective2_def)
   prf
   done
 
-subsection \<open>* Consistency *\<close>
+subsection ‹* Consistency *›
 
-(*lemma True *)
-  (* nitpick [satisfy, user_axioms,card i=2,  show_all, expect = genuine, timeout =100] oops *)
+(*Proof found *)
+lemma True 
+  nitpick [satisfy, user_axioms, card i=2, expect = genuine] oops 
       
-subsection \<open>* HOL *\<close>
+subsection ‹* HOL *›
 
 (*Proof found,/nitpick failed *)
-theorem Pred: "\<lfloor>\<^bold>\<exists>\<Phi>.( \<Phi> x \<^bold>\<leftrightarrow>  (\<^bold>\<exists>\<^sup>Ey. (x \<^bold>=\<^sup>L y) ))\<rfloor>"
+theorem Pred: "⌊❙∃Φ.( Φ x ❙↔  (❙∃⇧Ey. (x ❙=⇧L y) ))⌋"
   using nonempty by blast
 
 (*Proof found,/nitpick failed *)    
-theorem Taut: " L \<^bold>\<turnstile> p \<Longrightarrow> \<lfloor>\<^bold>\<exists>\<Phi>.( \<Phi> \<^bold>\<leftrightarrow>  p )\<rfloor>"
+theorem Taut: " L ❙⊢ p ⟹ ⌊❙∃Φ.( Φ ❙↔  p )⌋"
   using nonempty by blast
     
-section \<open>* Perfective- satisying A1 A2 *\<close>    
+section ‹* Perfective- satisying A1 A2 *›    
 
-(*Nitpick failed after 41 of the total and timeout 1300 *)
-(*Vampire found a proof- using perfective_def nonempty by presburger *)
-lemma A1CF: "\<lfloor>\<^bold>\<forall>\<Phi>. (((Perfective \<Phi>) \<^bold>\<rightarrow> \<^bold>\<not> (Perfective (\<^sup>\<not>\<Phi>)) )  )\<rfloor>"
-  using Perfective1_def Perfective2_def PerfConj CF1 CF2 CF3 CF4 by metis
 
-(*Vampire found a proof-  by (metis (no_types, lifting) CF1 CF3)*)
-lemma A2CF1: "\<lfloor>\<^bold>\<forall>\<Phi>.( \<^bold>\<forall>\<Psi>.( ( (Perfective \<Phi>) \<^bold>\<and> \<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( \<Phi> x \<^bold>\<rightarrow> \<Psi> x))) \<^bold>\<rightarrow> Perfective1 \<Psi>))\<rfloor>"
-  using Perfective_def Perfective1_def CF1 CF2 CF3 CF4 by (metis (no_types, lifting) CF1 CF3)
+(*Vampire found a proof- using Perfective_def nonempty by presburger *)
+lemma A1CF: "⌊❙∀Φ. (((Perfective Φ) ❙→ ❙¬ (Perfective (⇧¬Φ)) )  )⌋"
+  using Perfective1_def Perfective2_def PerfConj CF1 CF2 CF3 by metis
 
-lemma A2CF2: "\<lfloor>\<^bold>\<forall>\<Phi>.( \<^bold>\<forall>\<Psi>.( ( (Perfective \<Phi>) \<^bold>\<and> \<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( \<Phi> x \<^bold>\<rightarrow> \<Psi> x))) \<^bold>\<rightarrow>   \<^bold>\<not>(\<^bold>\<box>(  \<^bold>\<not>(Perfective2 \<Psi>) ))) )\<rfloor>"
+(*proof -
+  have "sK0 ❙⊨ mexistsB eiw"
+    by (simp add: nonempty)
+  then have "(∃m. sK0 ❙⊨ (Perfective sK1 ❙∧ eiw m)) ∨ ⌊λi. ∀p. i ❙⊨ Perfective p ⟶ i ❙⊨ ❙¬ (Perfective (λm i. i ❙⊨ (⇧¬p) m))⌋"
+    by metis (* > 1.0 s, timed out *)
+  moreover
+  { assume "∃m. sK0 ❙⊨ (Perfective sK1 ❙∧ eiw m)"
+    then have ?thesis
+      by (smt (z3) Perfective_def) (* > 1.0 s, timed out *) }
+  ultimately show ?thesis
+    by argo
+qed *)
+
+
+axiomatization where CF4: "∀w. {w} = Lew_Minset w (❙¬ ⊥)"
+
+lemma A2CF1: "⌊❙∀Φ.( ❙∀Ψ.( ( (Perfective Φ) ❙∧ ❙□ (❙∀⇧Ex.( Φ x ❙→ Ψ x))) ❙→ Perfective1 Ψ))⌋"
+  using  Perfective_def Perfective1_def CF1 CF2 CF3 CF4 by presburger
+  (*nitpick[user_axioms] oops *)
+
+
+lemma A2CF2: "⌊❙∀Φ.( ❙∀Ψ.( ( (Perfective Φ) ❙∧ ❙□ (❙∀⇧Ex.( Φ x ❙→ Ψ x))) ❙→   ❙¬(❙□(  ❙¬(Perfective2 Ψ) ))) )⌋"
   using Perfective_def Perfective2_def CF1 CF2 CF3 CF4 by metis
-    
-(*Nitpick found a countermodel*)    
-lemma Giv: "\<lfloor>Perfective1 DL\<rfloor>"
-  nitpick[user_axioms] oops
-   
+
+lemma A2CF23: "⌊❙∀Φ.( ❙∀Ψ.( ( (Perfective Φ) ❙∧ ❙□ (❙∀⇧Ex.( Φ x ❙→ Ψ x))) ❙→  (Perfective2 Ψ)) )⌋"
+  using Perfective_def Perfective2_def CF1 CF2 CF3 CF4 by metis
+
+        
+lemma Giv2: "⌊Perfective2 DL⌋"
+  using Perfective2_def DL_def CF3 CF1 by blast
+
+lemma Giv1: "⌊Perfective1 DL⌋"
+  using Perfective1_def DL_def CF3 CF1 by metis
+
 
 (*Section:  Perfective V2*) 
 
-consts OP :: "\<mu> \<Rightarrow> \<sigma>"
+consts OP :: "μ ⇒ σ"
 
-definition DN :: "\<mu> \<Rightarrow> \<sigma>" where
-  " DN (x) \<equiv> \<^bold>\<not>( (\<lambda>y.  (\<^bold>\<box> ( OP(y)) ))(x)  )"
+definition DN :: "μ ⇒ σ" where
+  " DN (x) ≡ ❙¬( (λy.  (❙□ ( OP(y)) ))(x)  )"
   
-definition DNL :: "\<mu> \<Rightarrow> \<sigma>" where
-  " DNL (x) \<equiv> \<^bold>\<not>(DN(x))"  
+definition DNL :: "μ ⇒ σ" where
+  " DNL (x) ≡ ❙¬(DN(x))"  
     
-definition Pn1 ::  "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where 
-    "  Pn1 (\<phi>)  \<equiv> ( \<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( (\<^bold>\<not> (\<phi>(x))) \<^bold>\<rightarrow>  DN(x) ) ))"  
+definition Pn1 ::  "(μ ⇒ σ) ⇒ σ" where 
+    "  Pn1 (φ)  ≡ ( ❙□ (❙∀⇧Ex.( (❙¬ (φ(x))) ❙→  DN(x) ) ))"  
     
-definition Pn2 ::  "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where 
-    "  Pn2 (\<phi>)  \<equiv>  \<^bold>\<not> (\<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( \<phi>(x) \<^bold>\<rightarrow>  DN(x) )))"      
+definition Pn2 ::  "(μ ⇒ σ) ⇒ σ" where 
+    "  Pn2 (φ)  ≡  ❙¬ (❙□ (❙∀⇧Ex.( φ(x) ❙→  DN(x) )))"      
   
-definition PerfectiveV2 ::  "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where 
-    "  PerfectiveV2 (\<phi>)  \<equiv> ( \<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( (\<^bold>\<not> (\<phi>(x))) \<^bold>\<rightarrow>  DN(x) ) ))  \<^bold>\<and>   \<^bold>\<not> (\<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( \<phi>(x) \<^bold>\<rightarrow>  DN(x) )))"
+definition PerfectiveV2 ::  "(μ ⇒ σ) ⇒ σ" where 
+    "  PerfectiveV2 (φ)  ≡ ( ❙□ (❙∀⇧Ex.( (❙¬ (φ(x))) ❙→  DN(x) ) ))  ❙∧   ❙¬ (❙□ (❙∀⇧Ex.( φ(x) ❙→  DN(x) )))"
 
 
-lemma PN1Attempt2: "\<lfloor>PerfectiveV2(\<phi>) \<^bold>\<rightarrow> (Pn1 (\<lambda>x. \<^bold>\<box> (\<phi>(x)) ))\<rfloor>"
+lemma PN1Attempt2: "⌊PerfectiveV2(φ) ❙→ (Pn1 (λx. ❙□ (φ(x)) ))⌋"
 nitpick [user_axioms, show_all] oops
 
 
-lemma PN2Attempt2: "\<lfloor>PerfectiveV2(\<phi>) \<^bold>\<rightarrow> (Pn2 (\<lambda>x. \<^bold>\<box> (\<phi>(x)) ))\<rfloor>"
+lemma PN2Attempt2: "⌊PerfectiveV2(φ) ❙→ (Pn2 (λx. ❙□ (φ(x)) ))⌋"
  nitpick [user_axioms] oops
 
 
